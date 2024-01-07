0e7808cb650935457ae22917107cfae0d451fd87
Dan Zaharia <zaharia.danny@gmail.com>
2024-01-06 22:59:06 -0500

    create a way to read the switches and send the corresponding command to home assistant

--- code.py
+++ code.py
@@ -9,14 +9,32 @@ from math import floor
 import ui
 import feeds
 from feeds import mqtt_client
+from adafruit_seesaw.seesaw import Seesaw
+from adafruit_seesaw.digitalio import DigitalIO
+import digitalio
+from adafruit_seesaw.pwmout import PWMOut
 
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 temp_probe = adafruit_htu31d.HTU31D(i2c_bus)
 temp_probe.heater = False
 lightSensor = AnalogIn(board.LIGHT)
 
+# set up the switches
+leftButtonBoard = Seesaw(i2c_bus, 0x3A)
+rightButtonBoard = Seesaw(i2c_bus, 0x3B)
+buttonBoards = (leftButtonBoard, rightButtonBoard)
+buttonPins = (18, 19, 20, 2)
+buttons = []
+for butonhBoard in buttonBoards:
+    for buttonPin in buttonPins:
+        button = DigitalIO(buttonBoards, buttonPin)
+        button.direction = digitalio.Direction.INPUT
+        button.pull = digitalio.Pull.UP
+        buttons.append(button)
+
+
 lastButtonPush = 0.0
-def checkButtons():
+def checkTouchScreen():
     global lastButtonPush
     point = ui.ts.touch_point
 
@@ -63,6 +81,20 @@ def checkButtons():
                     ui.set_backlight(1)
         time.sleep(.075)
 
+def checkButtons():
+    for i, button in buttons:
+        if button.value:
+            ui.enableScreen()
+            lastButtonPush = time.monotonic()
+            if i == 0:
+                ui.updateTemperature(ui.temperatureSetting + 1)
+                feeds.publish(feeds.temperatureSettingFeedCommand, ui.temperatureSetting)
+            elif i == 1:
+                ui.updateTemperature(ui.temperatureSetting - 1)
+                feeds.publish(feeds.temperatureSettingFeedCommand, ui.temperatureSetting)
+            else:
+                feeds.publish(feeds.commander, i - 1)
+            
 
 def checkTemperature():
     print("Check readings")
@@ -140,7 +172,7 @@ feeds.publish(feeds.fanToggleFeed, ui.fanToggle)
 prev_refresh_time = 0.0
 while True:
     # print("Main loop")
-    checkButtons()
+    checkTouchScreen()
     if (time.monotonic() - lastButtonPush) > 15 :
         ui.disableScreen()
     if (time.monotonic() - prev_refresh_time) > 40:
